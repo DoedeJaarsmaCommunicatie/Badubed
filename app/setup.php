52f21ea5975f5455184de02584d456c341b0314c
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2018-11-28 11:06:32 +0100

    first look at child theme, then parent
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/setup.php
+++ app/setup.php
@@ -126,13 +126,13 @@ add_action( 'widgets_init', 'badubed_widgets_init' );
 function badubed_scripts() {
 	wp_enqueue_style( 'badubed-style', get_stylesheet_uri() );
 	
-	wp_enqueue_style( 'badubed-main-style', get_template_directory() . '/dist/styles/main.css' );
+	wp_enqueue_style( 'badubed-main-style', get_stylesheet_uri() . '/dist/styles/main.css' );
 	
-	wp_enqueue_script( 'main', get_template_directory_uri() . '/dist/scripts/main.js', ['jquery'], null, true );
+	wp_enqueue_script( 'main', get_stylesheet_uri() . '/dist/scripts/main.js', ['jquery'], null, true );
 	
-	wp_enqueue_script( 'badubed-navigation', get_template_directory_uri() . '/dist/scripts/navigation.js', [], '20151215', true );
+	wp_enqueue_script( 'badubed-navigation', get_stylesheet_uri() . '/dist/scripts/navigation.js', [], '20151215', true );
 	
-	wp_enqueue_script( 'badubed-skip-link-focus-fix', get_template_directory_uri() . '/dist/scripts/skip-link-focus-fix.js', [], '20151215', true );
+	wp_enqueue_script( 'badubed-skip-link-focus-fix', get_stylesheet_uri() . '/dist/scripts/skip-link-focus-fix.js', [], '20151215', true );
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
